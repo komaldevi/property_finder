0e625c5d8ea8fb728216c63c891a09f88b7f1e2b
komaldevi <devikomal9607@gmail.com>
2024-01-01 00:41:37 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out' , to: 'devise/sessions#destroy'
+
+  end
+  devise_for :users
+
+
+  get 'home/about'
+  get 'post/myposts'
+  resources :posts
+  root "posts#index"
+
+end
---
+++ db/migrate/20231215054152_adduseridtoposts.rb
@@ -0,0 +1,4 @@
+class Adduseridtoposts < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
